2f1de07dd6ba95c1d949c3d9c22c2de3ab80d497
natalyq1 <onatylili@gmail.com>
2023-05-06 21:29:23 -0500

    getting access to each post by apart

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './assets/css/base/base.css';
 import './assets/css/componentes/card.css'
 import Header from './components/Header';
 import Home from './pages/Home'
+import Post from './pages/Post';
 import Sobre from './pages/Sobre'
 import Page404 from './pages/page404';
 
@@ -18,6 +19,7 @@ function App() {
       {/*Dinamico*/}
         <Route path='/' element={<Home />} />
         <Route path='/sobre' element={<Sobre />} />
+        <Route path='/posts/:id' element={<Post />} />
         <Route path='*' element={<Page404 />} />
       </Routes>
     </Router>
