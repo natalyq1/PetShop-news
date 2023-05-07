6591afb8e0eb25dc6118a562aa92f7c260e1ce64
natalyq1 <onatylili@gmail.com>
2023-05-06 19:06:59 -0500

    Setting up project

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './assets/css/base/base.css';
+import './assets/css/componentes/card.css'
+import Home from './pages/Home'
+import Sobre from './pages/Sobre'
+function App() {
+
+  const location = window.location
+  console.log(location);
+
+  const Router = () => {
+    if (location === '/'){
+      return <Home />
+    }else{
+      return <Sobre />
+    }
+  }
+
+  return (
+    <>
+    
+   {Router()}
+
+    </>
+  );
+}
+
+export default App;
